4caf16f7c229260e040ed62aefa775cede573f26
menzelths <menzelths@gmail.com>
2018-04-16 10:24:06 +0200

    Initial commit

---
+++ src/bildmanipulator/BildManipulator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bildmanipulator;
+
+/**
+ *
+ * @author tommy
+ */
+public class BildManipulator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Fenster f=new Fenster();
+        f.setVisible(true);
+    }
+}
